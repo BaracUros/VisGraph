d0ebd07df97417ecfb9ce98cd5ab5fd7adeffa61
Uroš Barać <uros.barac6@gmail.com>
2021-06-21 21:09:40 +0200

    Update VisGraph.py

--- vis_graph/VisGraph.py
+++ vis_graph/VisGraph.py
@@ -22,32 +22,33 @@ class VisGraph:
             raise ValueError("Inputs are different length")
     
     def get_graph(self):
-        
+        edges = []
         if self.directed == True:
             for i in range(len(self.mag) - 1):
-                if (self.mag[i + 1] - self.mag[i]) / (self.time[i + 1] - self.time[i]) > 0:
-                    self.graph.add_edge(i + 1, i)
+                if self.mag[i + 1] > self.mag[i] > 0:
+                    edges.append([i + 1, i])
                 else:
-                    self.graph.add_edge(i, i + 1)
+                    edges.append([i, i + 1])
             for i in range(len(self.mag)):
                 for j in range(i + 2, len(self.mag), 1):
                     line = self.mag[j] + (self.mag[i] - self.mag[j]) * (self.time[j] - self.time[i + 1:j]) / (self.time[j] - self.time[i])
                     values = self.mag[i + 1:j]
                     if np.all(line - values > 0):
                         if self.mag[j] > self.mag[i]:
-                            self.graph.add_edge(j, i)
+                            #self.graph.add_edge(j, i)
+                            edges.append([j, i])
                         else:
-                            self.graph.add_edge(i, j)
+                            #self.graph.add_edge(i, j)
+                            edges.append([i, j])
         else:
             for i in range(len(self.mag) - 1):
-                self.graph.add_edge(i, i + 1)
+                edges.append([i, i + 1])
             for i in range(len(self.mag)):
                 for j in range(i + 2, len(self.mag), 1):
                     line = self.mag[j] + (self.mag[i] - self.mag[j]) * (self.time[j] - self.time[i + 1:j]) / (self.time[j] - self.time[i])
                     values = self.mag[i + 1:j]
                     if np.all(line - values > 0):
-                        self.graph.add_edge(i, j)
+                        edges.append([i, j])
+        self.graph.add_edges_from(edges)
                   
         return self.graph
-
-
